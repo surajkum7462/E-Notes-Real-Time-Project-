8dad25ed03d60cd2deebd57e2c5cedd3a07fd118
surajkumar <kumarsuraj7462998828@gmail.com>
2025-03-25 23:12:56 +0530

    save get category implement

---
+++ src/main/java/com/suraj/controller/CategoryController.java
@@ -0,0 +1,46 @@
+package com.suraj.controller;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.suraj.entity.Category;
+import com.suraj.service.CategoryService;
+
+@RestController
+@RequestMapping("/api/v1/category")
+public class CategoryController {
+
+	@Autowired
+	private CategoryService categoryService;
+
+	@PostMapping("/save-category")
+	public ResponseEntity<?> saveCategory(@RequestBody Category category) {
+		Boolean saveCategory = categoryService.saveCategory(category);
+		if (saveCategory) {
+			return new ResponseEntity<>("Saved Successfully", HttpStatus.CREATED);
+		} else {
+			return new ResponseEntity<>("Not Saved", HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	@GetMapping("/category")
+	public ResponseEntity<?> getCategory() {
+		List<Category> allCategory = categoryService.getAllCategory();
+		if (CollectionUtils.isEmpty(allCategory)) {
+			return ResponseEntity.noContent().build();
+		} else {
+			return new ResponseEntity<>(allCategory, HttpStatus.OK);
+		}
+	}
+
+}
---
+++ src/main/java/com/suraj/entity/Category.java
@@ -0,0 +1,35 @@
+package com.suraj.entity;
+
+import java.util.Date;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Category extends BaseModel{
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	private String name;
+	
+	private String description;
+	
+	
+	
+	
+
+}

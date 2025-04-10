ad3325fb1f7ed011514840d1bb10c6f095e171d5
surajkumar <kumarsuraj7462998828@gmail.com>
2025-04-10 11:00:51 +0530

    update

--- src/main/java/com/suraj/config/security/SecurityConfig.java
+++ src/main/java/com/suraj/config/security/SecurityConfig.java
@@ -61,7 +61,7 @@ public class SecurityConfig {
 	{
 		http.csrf(csrf->csrf.disable())
 		.authorizeHttpRequests(req->
-		req.requestMatchers("/api/v1/home/**","/api/v1/auth/**","/swagger-ui/**","/v3/api-docs/**","/enotes-doc/**","/enotes-api-docs/**").permitAll()
+		req.requestMatchers("/api/v1/home/**","/api/v1/auth/**","/swagger-ui/**","/v3/api-docs/**","/enotes-doc/**","/enotes-api-docs/**","/actuator/**").permitAll()
 		.anyRequest().authenticated())
 		
 		.httpBasic(Customizer.withDefaults())
